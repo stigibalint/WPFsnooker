14370c3c63e966891dfe7d8052972e6717754f0c
stigibalint <72031994+stigibalint@users.noreply.github.com>
2023-12-08 11:07:34 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfSnooker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Versenyzo> adatok = new List<Versenyzo>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgTablazat.ItemsSource = adatok;
+            File.ReadAllLines("snooker.txt").Skip(1).ToList().ForEach(x => adatok.Add(new Versenyzo(x)));
+            List<string> orszagok = new List<string>();
+            adatok.GroupBy(x => x.Orszag).ToList().ForEach(x => orszagok.Add(x.Key));
+            cbOrszagok.ItemsSource = orszagok;
+            cbOrszagok.SelectedIndex = 0;
+        }
+       private void btnF3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"A világranglistán{ adatok.Count()} versenyző szerepel");
+        }
+        private void btnF4_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{Math.Round(adatok.Average(x => x.Nyeremeny), 2)}");
+        }
+        private void btnF5_Click(object sender, RoutedEventArgs e)
+        {
+            var legjobbanKereso = adatok.Where(x => x.Orszag == cbOrszagok.SelectedItem).OrderByDescending(x => x.Nyeremeny).First();
+            lblHelyezes.Content = legjobbanKereso.Helyezes;
+            lblNev.Content = legjobbanKereso.Nev;
+            lblOrszag.Content = legjobbanKereso.Orszag;
+            lblNyeremeny.Content = legjobbanKereso.Nyeremeny * int.Parse(txtArfolyam.Text);
+        }
+        private void btnF6_Click(object sender, RoutedEventArgs e)
+        {
+          var vanIlyenOrszag =  adatok.Select(x => x.Orszag).Contains(txtVanIlyenOrszag.Text);
+            if (vanIlyenOrszag == true)
+                MessageBox.Show("Van ilyen ország");
+            else
+                MessageBox.Show("Nincs ilyen ország");
+       
+        }
+
+        private void btnF7_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> orszagonkentVersenyzo = new List<string>();
+            adatok.GroupBy(x => x.Orszag).Where(x => x.Count() > sliMinLetszam.Value).ToList().ForEach(x=> orszagonkentVersenyzo.Add($"{x.Key} : {x.Count()} fő"));
+            lbStatisztika.ItemsSource = orszagonkentVersenyzo;
+        }
+    }
+}
---
+++ Versenyzo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSnooker
+{
+    public class Versenyzo
+    {
+        int helyezes;
+        string nev;
+        string orszag;
+        int nyeremeny;
+
+
+        public Versenyzo(string csvLine)
+        {
+            string[] values = csvLine.Split(';');
+            this.helyezes = int.Parse(values[0]);
+            this.nev = values[1];
+            this.orszag = values[2];
+            this.nyeremeny = int.Parse(values[3]);
+        }
+
+        public Versenyzo(int helyezes, string nev, string orszag, int nyeremeny)
+        {
+            this.helyezes = helyezes;
+            this.nev = nev;
+            this.orszag = orszag;
+            this.nyeremeny = nyeremeny;
+        }
+
+        public int Helyezes { get => helyezes; }
+        public string Nev { get => nev; }
+        public string Orszag { get => orszag; }
+        public int Nyeremeny { get => nyeremeny; }
+    }
+}
+
